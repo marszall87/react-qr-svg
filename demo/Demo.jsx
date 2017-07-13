dd61fe87335dd827905b01c84f78a9c73b74600f
Michał Nykiel <michal.nykiel@spartez.com>
2017-07-13 22:48:31 +0200

    add logo

--- demo/Demo.jsx
+++ demo/Demo.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import { QRCode } from '../src/index';
 
+const logo = (
+  <svg height="9" viewBox="-20 -30 220 220" width="9">
+    <g fill="none" fillRule="evenodd" stroke="none" stroke-Width="1">
+        <g transform="translate(45.000000, 79.000000)">
+            <polygon fill="#000000" points="45.1 0.1 0 78.2 90.3 78.2 135.4 0"/>
+        </g>
+        <g transform="translate(30.000000, 53.000000)">
+            <polygon fill="#FFFFFF" points="0.0501708984 78.1858521 8 91.2 98.3 91.2 143.4 13 135.327159 0.0268729573"/>
+            <polygon fill="#000000" points="45.1 0.1 0 78.2 90.3 78.2 135.4 0"/>
+        </g>
+        <g>
+            <polygon fill="#FFFFFF" points="0.0352172852 78.7047729 8 92.2 98.3 92.2 143.4 14 135.446439 0.51429416"/>
+            <polygon fill="#000000" points="45.1 0.6 0 78.7 90.3 78.7 135.4 0.5"/>
+        </g>
+    </g>
+</svg>
+);
+
 class Demo extends React.Component {
   constructor(props, context) {
     super(props, context);
@@ -50,6 +68,8 @@ class Demo extends React.Component {
         <QRCode
           bgColor={state.bgColor}
           fgColor={state.fgColor}
+          image={logo}
+          imageSize={9}
           level={state.level}
           style={{ width: 500 }}
           value={state.value}
--- src/components/qr-svg.js
+++ src/components/qr-svg.js
@@ -4,11 +4,21 @@ import PropTypes from 'prop-types';
 const QRCodeImpl = require('qr.js/lib/QRCode');
 const ErrorCorrectLevel = require('qr.js/lib/ErrorCorrectLevel');
 
+const imagePosition = (qrSize, imageSize) => (qrSize - imageSize) / 2;
+
+const isImageAreaForSize = (qrSize, imageSize) => (col, row) => {
+  const min = imagePosition(qrSize, imageSize);
+  const max = min + imageSize;
+  return (col >= min && col < max && row >= min && row < max);
+}
+
 export function QRCode({
     value = '',
     level = 'L',
     bgColor = '#FFFFFF',
     fgColor = '#000000',
+    imageSize = 7,
+    image,
     ...otherProps
 } = {}) {
     // adapted from https://github.com/zpao/qrcode.react/blob/master/src/index.js
@@ -17,26 +27,34 @@ export function QRCode({
     qrcode.make();
 
     const cells = qrcode.modules;
+    const position = imagePosition(cells.length, imageSize);
+    const isImageArea = isImageAreaForSize(cells.length, imageSize);
 
     return (<svg shapeRendering="crispEdges" viewBox={[0, 0, cells.length, cells.length].join(' ')} {...otherProps}>
         {
             cells.map((row, rowIndex) =>
-                row.map((cell, colIndex) => (
-                    <rect height={1}
+                row.map((cell, colIndex) => {
+                  if (isImageArea(colIndex, rowIndex)) {
+                    return null;
+                  }
+                  return (<rect height={1}
                         style={{ fill: cell ? fgColor : bgColor }}
                         width={1}
                         x={colIndex}
                         y={rowIndex}
-                    />
-                )))
+                    />)
+                }))
         }
+        <g transform={`translate(${position} ${position})`}>{image}</g>
     </svg>);
 }
 
 QRCode.propTypes = {
-    value: PropTypes.string.isRequired,
-    size: PropTypes.number,
-    level: PropTypes.oneOf(['L', 'M', 'Q', 'H']),
     bgColor: PropTypes.string,
     fgColor: PropTypes.string,
+    image: PropTypes.element,
+    imageSize: PropTypes.number,
+    level: PropTypes.oneOf(['L', 'M', 'Q', 'H']),
+    size: PropTypes.number,
+    value: PropTypes.string.isRequired,
 };
